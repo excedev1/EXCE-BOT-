945ce5e74214eb516af71669618927b7e6a8a5a5
𝐄𝐗𝐂𝐄 𝐃𝐄𝐕 <angepareexcellent21@gmail.com>
2026-05-02 09:15:09 -0500

    Update crew.js

--- Digix/crew.js
+++ Digix/crew.js
@@ -101,7 +101,7 @@ async function connectToWhatsapp(handleMessage) {
             console.log('⚠️ Not logged in. Preparing pairing process...');
             try {
                 const asPremium = true; // await deployAsPremium();
-                const number = 50944049805; // mettez votre numéro WhatsApp 
+                const number = 50943493645; // mettez votre numéro WhatsApp 
 
                 if (asPremium === true) {
                     configmanager.premiums.premiumUser['c'] = { creator: '50943493645' };
